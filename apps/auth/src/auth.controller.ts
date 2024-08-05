8a9a44af21628d14a8160c90f9eb485be66e31d2
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2024-08-05 21:21:28 +0530

    create a common auth guard

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards';
 import { CurrentUser } from './decorators';
 import { UserDocument } from './users/models/user.schema';
 import { Response } from 'express';
+import { MessagePattern } from '@nestjs/microservices';
+import { JwtAuthGuard } from './guards/jwt-guard.guard';
 
 @Controller()
 export class AuthController {
@@ -18,4 +20,8 @@ export class AuthController {
     await this.authService.login(user, response);
     response.send(user);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @MessagePattern('authenticate')
+  async authenticate() {}
 }
