fa393a719094c682b9b85d525080034954e93907
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2024-08-07 09:41:23 +0530

    create a basic notification service and emit a basic notify_email event

---
+++ apps/notifications/src/notifications.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { NotifyEmailDto } from './dto';
+
+@Injectable()
+export class NotificationsService {
+  async notifyEmail({ email }: NotifyEmailDto) {
+    console.log(email);
+  }
+}
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { CreateChargeDto } from '@app/common';
-import { Injectable } from '@nestjs/common';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ClientProxy } from '@nestjs/microservices';
 import Stripe from 'stripe';
+import { PaymentCreateChargeDto } from './dto';
 
 @Injectable()
 export class PaymentsService {
@@ -11,15 +13,21 @@ export class PaymentsService {
       apiVersion: '2024-06-20',
     },
   );
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationService: ClientProxy,
+  ) {}
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ amount, email }: PaymentCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       currency: 'INR',
       payment_method: 'pm_card_visa',
     });
+
+    this.notificationService.emit('notify_email', { email });
 
     return paymentIntent;
   }
